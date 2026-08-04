19ee199662d3e7d698f7d756440ca47cd9f6cd55
axlrose- <arjkris789@gmail.com>
2026-08-04 19:19:46 +0530

    changes in bat-notif

--- scripts/battery-notif.sh
+++ scripts/battery-notif.sh
@@ -3,6 +3,14 @@
 status=$(cat /sys/class/power_supply/BAT1/status)
 charge=$(cat /sys/class/power_supply/BAT1/capacity)
 
+# acts to check if already notified
+tmp="/tmp/bat-check-file"
+
 if [[ $status == "Discharging" && $charge -le 20 ]]; then
-	notify-send -u critical "Battery Low" "Battery lower than 20%"
+	if [[ ! -f "$tmp" ]]; then
+		notify-send -u critical "Battery Low" "Battery lower than 20%"
+		touch "$tmp"
+	fi
+else
+	rm -f "$tmp"
 fi
